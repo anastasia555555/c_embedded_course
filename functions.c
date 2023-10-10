bdb19621efb50f2fb690b84203003b08b91a82cc
Anastasia Kaliuzhna <103441373+anastasia555555@users.noreply.github.com>
2023-10-10 11:13:42 +0300

    Added comments
    Calculation of angles is in radians.

--- functions.c
+++ functions.c
@@ -9,8 +9,8 @@ int main()
     
     d = 14;
     alpha = 180;
-    double z1 = sin(PI / 2 + 3 * d) / (1 - sin(3 * alpha - PI));
-    double z2 = 1 / tan((5/4) * PI + (3/2) * alpha);
+    double z1 = sin(PI / 2 + 3 * d) / (1 - sin(3 * alpha - PI)); //sin excepts args in radians
+    double z2 = 1 / tan((5/4) * PI + (3/2) * alpha); //tan excepts args in radians
     
     printf("%lf\n", z1);
     printf("%lf\n", z2);
